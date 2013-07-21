be3f4b94e506a3dead6d6eaaaa9fe1f67470c4a2
Artyom Semyonov <sevenov@gmail.com>
2013-07-21 17:53:08 +0400

    Just typo fixes

--- priv/static/javascripts/application.js
+++ priv/static/javascripts/application.js
@@ -1,23 +1,21 @@
 ko.extenders.numeric = function(target, precision) {
-    var result = ko.computed({
-        read: target,
-        write: function(newValue) {
-            var current = target(),
-                roundingMultiplier = Math.pow(10, precision),
-                newValueAsNum = isNaN(newValue) ? 0 : parseFloat(+newValue),
-                valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
+  var result = ko.computed({
+    read: target,
+    write: function(newValue) {
+      var current = target(),
+          roundingMultiplier = Math.pow(10, precision),
+          newValueAsNum = isNaN(newValue) ? 0 : parseFloat(+newValue),
+          valueToWrite = Math.round(newValueAsNum * roundingMultiplier) / roundingMultiplier;
  
-            if (valueToWrite !== current) {
-                target(valueToWrite);
-            } else {
-                if (newValue !== current) {
-                    target.notifySubscribers(valueToWrite);
-                }
-            }
-        }
-    });
-    result(target());
-    return result;
+      if (valueToWrite !== current) {
+        target(valueToWrite);
+      } else if (newValue !== current) {
+        target.notifySubscribers(valueToWrite);
+      }
+    }
+  });
+  result(target());
+  return result;
 };
 
 
